b3a9015bb1a92455f44328556b1fddc9747644ba
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-12 04:09:25 +0100

    integrate new job routes into express application

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const sequelize = require('./config/database');
 const User = require('./models/User');
 const Job = require('./models/Job');
+const express = require('express');
+const jobRoutes = require('./routes/jobs');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/jobs', jobRoutes);
 
 sequelize.sync({ alter: true })
   .then(() => console.log("Database synchronized"))
