235c813ed21cb61c1334faae18a5d990dd0e4264
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-12 03:58:59 +0100

    Add route to fetch all jobs for a given user

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -26,4 +26,15 @@ router.post('/add', verifyToken, createUserIfNotExists, async (req, res) => {
   }
 });
 
+router.get('/', verifyToken, createUserIfNotExists, async (req, res) => {
+  try {
+    const jobs = await Job.findAll({
+      where: { userId: req.dbUser.id }
+    });
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching jobs', error });
+  }
+});
+
 module.exports = router;
