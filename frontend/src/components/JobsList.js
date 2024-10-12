6ffe0d7b8ac319f9195f8df9d484b926c4245e34
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-12 05:00:12 +0100

    Create component to display all jobs

---
+++ frontend/src/components/JobsList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import api from '../api/axios';
+import JobItem from './JobItem';
+
+const JobsList = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await api.get('/jobs');
+        setJobs(response.data);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      }
+    };
+    fetchJobs();
+  }, []);
+
+  return (
+    <div>
+      <h1>Your Jobs</h1>
+      <ul>
+        {jobs.map((job) => (
+          <JobItem key={job.id} job={job} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default JobsList;
