f8bd1e7a228ff7b9789513c6ddde0fb818eb08b0
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-12 08:09:53 +0100

    Get the server to start listening on a port

--- server/server.js
+++ server/server.js
@@ -10,6 +10,16 @@ const app = express();
 app.use(express.json());
 app.use('/api/jobs', jobRoutes);
 
-sequelize.sync({ alter: true })
-  .then(() => console.log("Database synchronized"))
-  .catch(err => console.log("Error synchronizing database", err));
+ // Synchronize database
+sequelize.sync()
+.then(() => {
+  console.log('Database synchronized');
+  // Start the server after syncing
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((error) => {
+  console.error('Unable to synchronize database:', error);
+});
