25a6d9058af06397f9c3397d238389afb1d224d7
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-12 03:45:24 +0100

    setup firebase auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+module.exports = verifyToken;
