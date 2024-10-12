0a1ee3f48168da71d797403bebe23f72cc100654
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-12 08:26:03 +0100

    remove unused var

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -36,5 +35,4 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
