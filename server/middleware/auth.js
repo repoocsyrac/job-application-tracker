171c02f8fe0b386fce94f97377bb8bd47284cb0c
syrac <carys.cooper@univ.ox.ac.uk>
2024-10-13 13:39:37 +0100

    use firebase service account key

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,15 +1,26 @@
 const admin = require('firebase-admin');
-admin.initializeApp();
+//admin.initializeApp();
+
+var serviceAccount = require("../job-application-tracker-a65c5-firebase-adminsdk-lbrm9-9bacf85a62.json");
+
+admin.initializeApp({
+
+  credential: admin.credential.cert(serviceAccount)
+
+});
+
 
 const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
+  console.log("Received token by middleware:", token); // DEBUG
   if (!token) return res.status(401).json({ message: 'Unauthorized' });
-
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
+    console.log("Decoded token:", decodedToken); // Log token details for debugging
     req.user = decodedToken;
     next();
   } catch (error) {
+    console.error("Error verifying token:", error); // Log error details
     res.status(401).json({ message: 'Unauthorized' });
   }
 };
